347f0ce32362cf207ae8a1baeee95c4b087d3fc6
Grant Ramsay <seapagan@gmail.com>
2022-11-15 11:12:25 +0000

    initial commit
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

---
+++ README.md
@@ -0,0 +1,26 @@
+# Control PoE status on a Ubiquiti TS-8-Pro Switch
+
+**Development work** for a system to remotely and automatically control the PoE
+status of individual ports on a Ubiquiti TS-8-Pro Switch, using predefined
+profiles.
+
+## Use Cases
+
+- Control a set of IP cameras, switches and access points to enable disabling
+when not needed or quick enabling if required.
+
+## Development Plans
+
+Current proposed project plan.
+
+- [ ] Write proof-of-concept code to control ports.
+- [ ] Develop this into a full API (using FastAPI)
+- [ ] Create a command line app to interface with the above API
+- [ ] Create a Web App to interface with the above API
+
+## Contributing
+
+At this time, the project is barely in it's planning stage but I do have a firm
+idea where it's going and how to structure it. As such, other contributions are
+not looked for at this time. Hopefully, within a few days this project will be
+in a much more advance stage and that will change 😃.
---
+++ sshwrapper.py
@@ -0,0 +1,82 @@
+"""Custom class to encapsulate SSH access to devices."""
+import paramiko
+
+
+class Wrapper:
+    """SSH Wrapper Class.
+
+    Wraps an SSH connection, with methods to run remote commands, returning the
+    results or errors.
+    """
+
+    def __init__(self, host, username, password, port):
+        """Initialize the Class."""
+        self.host = host
+        self.username = username
+        self.password = password
+        self.port = port
+        self.client = paramiko.SSHClient()
+
+        # setup logging
+        # paramiko.util.log_to_file("paramiko.log")
+
+    def connect(self):
+        """Connect to a remote host via SSH.
+
+        This just opens the connection or raises an exception. The connection
+        should be closed by calling the close() method once the required
+        commands have been run
+        """
+        try:
+            # ignore missing keys with no warning
+            self.client.set_missing_host_key_policy(
+                paramiko.MissingHostKeyPolicy()
+            )
+            # open a connection to the specified host
+            self.client.connect(
+                self.host,
+                self.port,
+                self.username,
+                self.password,
+                look_for_keys=False,
+                allow_agent=False,
+                timeout=10,
+                banner_timeout=10,
+                auth_timeout=10,
+            )
+        except Exception as e:
+            print(f"*** Caught Connect exception: {e.__class__}: {e}")
+            try:
+                self.client.close()
+            except AttributeError:
+                pass
+            return
+
+    def close(self):
+        """Close the SSH connection to this host."""
+        self.client.close()
+
+    def run(self, cmd_str):
+        """Run a command on this host over the existing SSH connection.
+
+        Args:
+            cmd_str (string): [Command to run]
+
+        Returns:
+            output [string]: The output from the command
+            stderr [string]: Any errors from the remote stderr
+        """
+        try:
+            stdin, stdout, stderr = self.client.exec_command(
+                cmd_str, timeout=10
+            )
+
+            # convert the streams we want into strings.
+            err = stderr.read().decode("utf8").strip("\n")
+            out = stdout.read().decode("utf8").strip("\n")
+            # return the stdout and stderr as a tuple
+            return out, err
+        except Exception as e:
+            print(f"*** Caught Connect exception: {e.__class__}: {e}")
+            self.client.close()
+            return
